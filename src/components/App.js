05d3a7e8a5d376c90ec73cb1bdd1ee4cb045e3fe
hbseo <gusqja8753@gmail.com>
2020-09-19 21:13:05 +0900

    fabricjs

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { fabric } from 'fabric';
+import './App.css'
+
+class App extends Component {
+  componentDidMount() {
+    const canvas = new fabric.Canvas('canvas', {
+      preserveObjectStacking: true,
+      height: 1000,
+      width: 1000
+    });
+    canvas.backgroundColor = 'grey';
+    fabric.Image.fromURL('http://fabricjs.com/assets/pug_small.jpg', img => {
+      img.set({left:300, top:100});
+      img.scaleToWidth(300);
+      canvas.add(img);
+    })
+  }
+  render() {
+    return(
+      <div className='App'>
+        <h1>Image Editor</h1>
+        <ul>
+          <button>Undo</button>
+          <button>Redo</button>
+          <button>Rotate</button>
+          <button>Flip</button>
+          <button>Draw Line</button>
+          <button>Figure</button>
+          <button>Cut</button>
+          <button>Filter</button>
+          <button>Text</button>
+        </ul>
+        <canvas id='canvas'></canvas>
+      </div>
+    );
+  }
+}
+
+export default App;
