04ef1d1a1fc3bd1c7db326f69dbe14b75fccd997
3people <smsn7031@gmail.com>
2021-02-04 03:06:34 +0900

    Update Upload_file.js

--- src/components/Upload_file.js
+++ src/components/Upload_file.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../css/UploadFile.scss';
-
+// import '../css/UploadFile.scss';
 
 class Upload_file extends Component {
   constructor(props) {
