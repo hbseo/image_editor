a6560777a57ea5525d983703df9229bfc3b26550
JuKyYoon <yoongh97@gmail.com>
2021-06-01 14:53:35 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -36,8 +36,8 @@ app.use(express.static(path.join(__dirname, '../build')))
 // set the secret key variable for jwt
 app.set('jwt-secret', config.secret);
 
-const options = { etag: false };
-app.use(express.static("public", options));
+// const options = { etag: false };
+// app.use(express.static("public", options));
 
 // router
 // app.use((req, res, next) => {
@@ -51,15 +51,15 @@ app.use(express.static("public", options));
 app.use('/', require('./routes/index'));
 
 
-var host = process.env.HOST || '127.0.0.1';
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+// var host = process.env.HOST || '127.0.0.1';
+// var cors_proxy = require('cors-anywhere');
+// cors_proxy.createServer({
+//     originWhitelist: [], // Allow all origins
+//     requireHeader: ['origin', 'x-requested-with'],
+//     removeHeaders: ['cookie', 'cookie2']
+// }).listen(port, host, function() {
+//     console.log('Running CORS Anywhere on ' + host + ':' + port);
+// });
 
 // open server
 app.listen(port, () => {
